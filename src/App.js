b256a975cca2b1cf7755f4fa513bd10699401e13
poppan2 <ye_aung94@yahoo.com>
2021-10-28 08:44:15 -0700

    modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router";
+import "./App.css";
+import Pokedex from "./Components/Pokedex/Pokedex";
+import Team from "./Components/MyTeam/Team";
+import Header from "./Components/Header/Header";
+import Pokemon from "./Components/Pokemon/Pokemon";
+import { useState } from "react";
 
 function App() {
+  const [pokeData, setPokeData] = useState({});
+  const [addPokemon, setAddPokemon] = useState([]);
+
+  const handleClick = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setPokeData(data));
+  };
+
+  const addPokemonOnClick = (poke) => {
+    if(!addPokemon.includes(poke)){
+      setAddPokemon([...addPokemon, poke]);
+    }
+  };
+
+  // const removePokemonOnClick = (poke) => {
+  //   const team = [...addPokemon];
+  //   team.splice(team.indexOf(poke), 1);
+  //   setAddPokemon(team);
+  // };
+
+  const removePokemonOnClick = (poke) =>{
+    setAddPokemon([...addPokemon].filter((item)=>{
+      return item !== poke
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="main">
+        <Route
+          path="/pokedex"
+          render={() => <Pokedex handleClick={handleClick} />}
+        />
+
+        <Route
+          exact
+          path="/team"
+          render={() => (
+            <Team
+              addPokemon={addPokemon}
+              removePokemonOnClick={removePokemonOnClick}
+            />
+          )}
+        />
+
+        <Route
+          path="/pokedex/:pokename"
+          render={() => (
+            <Pokemon
+              pokeData={pokeData}
+              addPokemonOnClick={addPokemonOnClick}
+            />
+          )}
+        />
+      </main>
     </div>
   );
 }
